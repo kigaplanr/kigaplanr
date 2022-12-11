3798c3dddb5f51317a3f6789c15142ee82768a3a
Koni vKxni <eventbeate@gmail.com>
2022-12-11 19:17:45 +0100

    rename

--- lib/kigaplanr_web/controllers/parents_controller.ex
+++ lib/kigaplanr_web/controllers/parents_controller.ex
@@ -2,6 +2,6 @@ defmodule KigaplanrWeb.ParentsController do
   use KigaplanrWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "parents.html")
   end
 end
